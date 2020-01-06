9317fdf29e6963169d979aa2d92f9c4f9841c78d
Sabbir Hossain <smac@labs-mac.local>
2020-01-06 17:01:58 +0600

    Adding Dictionary

--- MultipleTableview/ViewController.swift
+++ MultipleTableview/ViewController.swift
@@ -10,11 +10,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var table1: UITableView!
+    @IBOutlet weak var table2: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        //Create Dictionary
+        
+        var myDictionary = ["A" : ["Apple","And"],
+                            "B" : ["Bubble","Blow"],
+                            "C" : ["Circle","Cup"],
+                            "D" : ["Do","Done"],
+                            "E" : ["Eagle","End"]]
+    
+        print(myDictionary)
+        
+      
     }
-
-
+    
 }
 

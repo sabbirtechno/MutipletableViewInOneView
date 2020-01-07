438e2aa282b3ace095941339eebf306cb1bb8b7d
Sabbir Hossain <smac@labs-mac.local>
2020-01-07 17:17:23 +0600

    Complete Code

--- MultipleTableview/ViewController.swift
+++ MultipleTableview/ViewController.swift
@@ -8,113 +8,89 @@
 
 import UIKit
 
-class ViewController: UIViewController,UITableViewDelegate,UITabBarDelegate {
-
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+  
+    var cellIndentifier : String = "cell"
+    
+    var numberOfRows :  Int = 0
+    
+    var item = [String]()
+    
     @IBOutlet weak var table1: UITableView!
     @IBOutlet weak var table2: UITableView!
     
     
-    let team : [String] = ["Arsenal","Chelsa","Manchester"]
-    
-    let ArsenalPlayers : [String] = ["Bangladesh","China","Canada"]
-    
-    let ChelsaPlayers: [String] = ["BMW","Lamborghini","Bucatti"]
-    
-    
-    let ManchesterPlayers : [String] = ["Yamaha","TVS","Honda"]
-    
-    var teamReference : String = "Country"
-    
-    
-    let cellIdentifier : String = "cell"
-    
-    var numberOfTeams : Int = 0
-    var numberOfPlayers : Int = 0
+    //Create Dictionary
+    let data = ["A" : ["Welcome","Sabbir","Hossain","Done"],
+                "B" : ["Bubble","Blow"],
+                "C" : ["Circle","Cup"],
+                "D" : ["Do","Done"],
+                "E" : ["Eagle","End"]]
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        
-        //Create Dictionary
-       // var myDictionary = ["A" : ["Apple","And"],
-                            //"B" : ["Bubble","Blow"],
-                            //"C" : ["Circle","Cup"],
-                            //"D" : ["Do","Done"],
-                            //"E" : ["Eagle","End"]]
-       // print(myDictionary)
-        
-        
-
         
         table1.delegate = self
-        table2.delegate = self
-        table1.dataSource = self as! UITableViewDataSource
-        table2.dataSource = self as! UITableViewDataSource
+        table1.delegate = self
+        table2.dataSource = self
+        table2.dataSource = self
         
+       // print(myDictionary)
         
-       func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         
-            if (tableView.tag == 1)
-            {
-                numberOfTeams = team.count
-                return numberOfTeams
-            }
-            else if (tableView.tag == 2)
-            
-            {
-               if (teamReference == "Arsenal")
-            {
-              numberOfPlayers = ArsenalPlayers.count
-                
-               }
-                else if (teamReference == "Chelsa")
-                {
-                    numberOfPlayers = ChelsaPlayers.count
-                }
-                else if (teamReference == "Manchester")
-                {
-                    numberOfPlayers = ManchesterPlayers.count
-                }
-                
-                return numberOfPlayers
-                
-                }
-                
-                else
-                {
-                    return 0
-                }
-
+        if tableView.tag == 1{
+            print("In tableView numberofrows")
+            print(data)
+            numberOfRows = data.count
+            print(numberOfRows)
+            return numberOfRows
         }
-
-    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell
-        {
-            
-            if(tableView.tag == 1){
-                cell.textLabel?.text = team[indexPath.row]
-            }
-            else if (tableView.tag == 2)
-            {
-                
-                if (teamReference == “Arsenal”)
-                    {
-                        cell.textLabel?.text = ArsenalPlayers[indexPath.row]
-                }
-                
-                else if (teamReference == "Chelsa")
-                {
-                    cell.textLabel?.text = ChelsaPlayers[indexPath.row]
-                }
-                else if (teamReference == "Manchester"){
-                    cell.textLabel?.text = ManchesterPlayer[index.row]
-                }
-                
-            }
-            
-            return cell
-            
+        else if tableView.tag == 2{
+            print("In tableView numberofrows")
+            print(data)
+            numberOfRows = item.count
+            return numberOfRows
         }
+        
+        else {
+            return 0
+        }
+    }
+    
+    
+    
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellIndentifier, for: indexPath as IndexPath)
+        if tableView.tag == 1{
+        cell.textLabel?.text = Array(data.keys)[indexPath.row]
             
+    }
+        else if tableView.tag == 2{
+            cell.textLabel?.text = item[indexPath.row]
+        }
+        
+     return cell
+        
 }
-
+    
+    
+    //For Click
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    if tableView.tag == 1 {
+    print(Array(data.values)[indexPath.row])
+    
+        item = Array(data.values)[indexPath.row]
+    }
+        
+        table2.reloadData()
+ }
+    
+    
+}
